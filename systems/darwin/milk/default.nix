f3be4675b98e8767f479a8a37619197167326ecd
Morgan Fudge <morgan.fudge@milkmoovement.com>
2025-02-03 15:00:55 -0330

    Fix ssh settings

--- systems/darwin/milk/default.nix
+++ systems/darwin/milk/default.nix
@@ -3,15 +3,8 @@
 
   environment.systemPackages = with pkgs; [ aws-vault ];
 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
-    };
-  };
-
-  users.users."${vars.user}".openssh.authorizedkeys.keys = [
+  services.openssh.enable = true;
+  users.users."${vars.user}".openssh.authorizedKeys.keys = [
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHyziKmJqEMI3C3/nV/lt32x0/ma1xfoKogmlHjbS+bjAAAADHNzaDp5dWJpa2V5NQ== ssh:yubikey5"
   ];
 
