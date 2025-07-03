bb1497919855cf294ef0e3422c971be8461cf05f
Morgan Fudge <morgan.fudge@milkmoovement.com>
2025-07-03 09:27:19 -0230

    Remove trampolines and move back to casks

--- systems/darwin/milk/default.nix
+++ systems/darwin/milk/default.nix
@@ -12,19 +12,8 @@
     colima
     docker-client
     docker-compose
-    zoom-us
     _1password-cli
   ];
-
-  # Setup trampolines to let spotlight find nix apps
-  system.activationScripts.postUserActivation.text = ''
-    apps_source="${config.system.build.applications}/Applications"
-    moniker="Nix Trampolines"
-    app_target_base="$HOME/Applications"
-    app_target="$app_target_base/$moniker"
-    mkdir -p "$app_target"
-    sudo ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
-  '';
 
   services.openssh.enable = true;
   users.users.${vars.user}.openssh.authorizedKeys.keys = [
@@ -43,6 +32,8 @@
       "1password"
       "slack"
       "postman"
+      "zoom"
+      "caffeine"
     ];
   };
 }
