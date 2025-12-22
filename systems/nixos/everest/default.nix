7b0eb58005ef42791aad89d34e0edd3488451045
Morgan Fudge <morgan@mkf.dev>
2025-12-21 23:28:54 -0330

    Add pipewire config

--- systems/nixos/everest/default.nix
+++ systems/nixos/everest/default.nix
@@ -79,6 +79,16 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire.noresample = {
+      "context.properties" = {
+        "default.clock.rate" = 192000;
+        "default.clock.allowed-rates" = [
+          44100
+          48000
+          192000
+        ];
+      };
+    };
   };
 
   # Needed for cooling
