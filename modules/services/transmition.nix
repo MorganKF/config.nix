bdd447a1b102198a7b7777e9854d512dd8f99f15
Morgan Fudge <morgan@mkf.dev>
2026-02-23 10:31:46 -0330

    Add transmission service UI

--- modules/services/transmition.nix
+++ modules/services/transmition.nix
@@ -7,6 +7,7 @@
       ...
     }:
     {
+      environment.systemPackages = with pkgs; [ transmission_4-qt ];
       services.transmission = {
         enable = true;
         package = pkgs.transmission_4;
