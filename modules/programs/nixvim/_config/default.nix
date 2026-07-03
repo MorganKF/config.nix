ba4878141c40d6a3474fd89fb507953d6f4e3e8c
MorganKF <morgan@mkf.dev>
2026-07-03 17:42:15 -0230

    feat: remove osc52 paste

--- modules/programs/nixvim/_config/default.nix
+++ modules/programs/nixvim/_config/default.nix
@@ -26,10 +26,6 @@
           ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
           ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
         },
-        paste = {
-          ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-          ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-        },
       }
     end
   '';
