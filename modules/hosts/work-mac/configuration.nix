e86d41f6213c8e9f07d29dbec8d5e5732b892697
MorganKF <morgan@mkf.dev>
2026-07-15 16:20:40 -0230

    feat(work-mac): add c# binaries

--- modules/hosts/work-mac/configuration.nix
+++ modules/hosts/work-mac/configuration.nix
@@ -7,6 +7,12 @@
         system-desktop
       ];
 
+      nixpkgs.config.permittedInsecurePackages = [
+        "dotnet-sdk-6.0.428"
+        "aspnetcore-runtime-6.0.36"
+        "dotnet-runtime-6.0.36"
+      ];
+
       environment.systemPackages = with pkgs; [
         opencode
         awscli2
@@ -14,6 +20,15 @@
         ssm-session-manager-plugin
         fnm
         bruno
+        (
+          with dotnetCorePackages;
+          combinePackages [
+            sdk_8_0
+            sdk_6_0
+            aspnetcore_6_0
+            runtime_6_0
+          ]
+        )
       ];
 
       system.stateVersion = "26.05";
